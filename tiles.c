f8d9454934e53d82b33086b79c798392c5d72fa1
mhinton2 <129218633+mhinton2@users.noreply.github.com>
2024-11-19 16:45:07 -0500

    Added 1 sprite that can move

--- tiles.c
+++ tiles.c
@@ -6,6 +6,9 @@
 /* include the image we are using */
 #include "background.h"
 
+/* include sprite image we are using */
+#include "koopa.h"
+
 /* include the tile map we are using */
 #include "map.h"
 #include "map2.h"
@@ -70,9 +73,25 @@ volatile short* bg3_y_scroll = (unsigned short*) 0x400001e;
 #define BUTTON_R (1 << 8)
 #define BUTTON_L (1 << 9)
 
+/* flags to set sprite handling in display control register */
+#define SPRITE_MAP_2D 0x0
+#define SPRITE_MAP_1D 0x40
+#define SPRITE_ENABLE 0x1000
+
+#define NUM_SPRITES 128
+
+/* memory location which controls sprite attributes */
+volatile unsigned short* sprite_attribute_memory = (volatile unsigned short*) 0x7000000;
+
 /* the scanline counter is a memory cell which is updated to indicate how
  * much of the screen has been drawn */
 volatile unsigned short* scanline_counter = (volatile unsigned short*) 0x4000006;
+
+/* the address of the color palettes used for sprites */
+volatile unsigned short* sprite_palette = (volatile unsigned short*) 0x5000200;
+
+/* the memory location which stores sprite image data */
+volatile unsigned short* sprite_image_memory = (volatile unsigned short*) 0x6010000;
 
 /* wait for the screen to be fully drawn so we can do something during vblank */
 void wait_vblank() {
@@ -107,6 +126,28 @@ volatile unsigned short* screen_block(unsigned long block) {
     return (volatile unsigned short*) (0x6000000 + (block * 0x800));
 }
 
+/* flag for turning on DMA */
+#define DMA_ENABLE 0x80000000
+
+/* flags for the sizes to transfer, 16 or 32 bits */
+#define DMA_16 0x00000000
+#define DMA_32 0x04000000
+
+/* pointer to the DMA source location */
+volatile unsigned int* dma_source = (volatile unsigned int*) 0x40000D4;
+
+/* pointer to the DMA destination location */
+volatile unsigned int* dma_destination = (volatile unsigned int*) 0x40000D8;
+
+/* pointer to the DMA count/control */
+volatile unsigned int* dma_count = (volatile unsigned int*) 0x40000DC;
+
+/* copy data using DMA */
+void memcpy16_dma(unsigned short* dest, unsigned short* source, int amount) {
+    *dma_source = (unsigned int) source;
+    *dma_destination = (unsigned int) dest;
+    *dma_count = amount | DMA_16 | DMA_ENABLE;
+}
 
 /* function to setup background 0 for this program */
 void setup_background() {
@@ -163,42 +204,305 @@ void delay(unsigned int amount) {
     for (int i = 0; i < amount * 10; i++);
 }
 
+/* a sprite is a moveable image on the screen */
+struct Sprite {
+    unsigned short attribute0;
+    unsigned short attribute1;
+    unsigned short attribute2;
+    unsigned short attribute3;
+};
+
+/* array of all the sprites available on the GBA */
+struct Sprite sprites[NUM_SPRITES];
+int next_sprite_index = 0;
+
+/* the different sizes of sprites which are possible */
+enum SpriteSize {
+    SIZE_8_8,
+    SIZE_16_16,
+    SIZE_32_32,
+    SIZE_64_64,
+    SIZE_16_8,
+    SIZE_32_8,
+    SIZE_32_16,
+    SIZE_64_32,
+    SIZE_8_16,
+    SIZE_8_32,
+    SIZE_16_32,
+    SIZE_32_64
+};
+
+/* function to initialize a sprite with its properties, and return a pointer */
+struct Sprite* sprite_init(int x, int y, enum SpriteSize size,
+        int horizontal_flip, int vertical_flip, int tile_index, int priority) {
+
+    /* grab the next index */
+    int index = next_sprite_index++;
+
+    /* setup the bits used for each shape/size possible */
+    int size_bits, shape_bits;
+    switch (size) {
+        case SIZE_8_8:   size_bits = 0; shape_bits = 0; break;
+        case SIZE_16_16: size_bits = 1; shape_bits = 0; break;
+        case SIZE_32_32: size_bits = 2; shape_bits = 0; break;
+        case SIZE_64_64: size_bits = 3; shape_bits = 0; break;
+        case SIZE_16_8:  size_bits = 0; shape_bits = 1; break;
+        case SIZE_32_8:  size_bits = 1; shape_bits = 1; break;
+        case SIZE_32_16: size_bits = 2; shape_bits = 1; break;
+        case SIZE_64_32: size_bits = 3; shape_bits = 1; break;
+        case SIZE_8_16:  size_bits = 0; shape_bits = 2; break;
+        case SIZE_8_32:  size_bits = 1; shape_bits = 2; break;
+        case SIZE_16_32: size_bits = 2; shape_bits = 2; break;
+        case SIZE_32_64: size_bits = 3; shape_bits = 2; break;
+    }
+
+    int h = horizontal_flip ? 1 : 0;
+    int v = vertical_flip ? 1 : 0;
+
+    /* set up the first attribute */
+    sprites[index].attribute0 = y |             /* y coordinate */
+                            (0 << 8) |          /* rendering mode */
+                            (0 << 10) |         /* gfx mode */
+                            (0 << 12) |         /* mosaic */
+                            (1 << 13) |         /* color mode, 0:16, 1:256 */
+                            (shape_bits << 14); /* shape */
+
+    /* set up the second attribute */
+    sprites[index].attribute1 = x |             /* x coordinate */
+                            (0 << 9) |          /* affine flag */
+                            (h << 12) |         /* horizontal flip flag */
+                            (v << 13) |         /* vertical flip flag */
+                            (size_bits << 14);  /* size */
+
+    /* setup the second attribute */
+    sprites[index].attribute2 = tile_index |   // tile index */
+                            (priority << 10) | // priority */
+                            (0 << 12);         // palette bank (only 16 color)*/
+
+    /* return pointer to this sprite */
+    return &sprites[index];
+}
+
+/* update all of the spries on the screen */
+void sprite_update_all() {
+    /* copy them all over */
+    memcpy16_dma((unsigned short*) sprite_attribute_memory, (unsigned short*) sprites, NUM_SPRITES * 4);
+}
+
+/* setup all sprites */
+void sprite_clear() {
+    /* clear the index counter */
+    next_sprite_index = 0;
+
+    /* move all sprites offscreen to hide them */
+    for(int i = 0; i < NUM_SPRITES; i++) {
+        sprites[i].attribute0 = HEIGHT;
+        sprites[i].attribute1 = WIDTH;
+    }
+}
+
+/* set a sprite postion */
+void sprite_position(struct Sprite* sprite, int x, int y) {
+    /* clear out the y coordinate */
+    sprite->attribute0 &= 0xff00;
+
+    /* set the new y coordinate */
+    sprite->attribute0 |= (y & 0xff);
+
+    /* clear out the x coordinate */
+    sprite->attribute1 &= 0xfe00;
+
+    /* set the new x coordinate */
+    sprite->attribute1 |= (x & 0x1ff);
+}
+
+/* move a sprite in a direction */
+void sprite_move(struct Sprite* sprite, int dx, int dy) {
+    /* get the current y coordinate */
+    int y = sprite->attribute0 & 0xff;
+
+    /* get the current x coordinate */
+    int x = sprite->attribute1 & 0x1ff;
+
+    /* move to the new location */
+    sprite_position(sprite, x + dx, y + dy);
+}
+
+/* change the vertical flip flag */
+void sprite_set_vertical_flip(struct Sprite* sprite, int vertical_flip) {
+    if (vertical_flip) {
+        /* set the bit */
+        sprite->attribute1 |= 0x2000;
+    } else {
+        /* clear the bit */
+        sprite->attribute1 &= 0xdfff;
+    }
+}
+
+/* change the vertical flip flag */
+void sprite_set_horizontal_flip(struct Sprite* sprite, int horizontal_flip) {
+    if (horizontal_flip) {
+        /* set the bit */
+        sprite->attribute1 |= 0x1000;
+    } else {
+        /* clear the bit */
+        sprite->attribute1 &= 0xefff;
+    }
+}
+
+/* change the tile offset of a sprite */
+void sprite_set_offset(struct Sprite* sprite, int offset) {
+    /* clear the old offset */
+    sprite->attribute2 &= 0xfc00;
+
+    /* apply the new one */
+    sprite->attribute2 |= (offset & 0x03ff);
+}
+
+/* setup the sprite image and palette */
+void setup_sprite_image() {
+    /* load the palette from the image into palette memory*/
+    memcpy16_dma((unsigned short*) sprite_palette, (unsigned short*) koopa_palette, PALETTE_SIZE);
+
+    /* load the image into sprite image memory */
+    memcpy16_dma((unsigned short*) sprite_image_memory, (unsigned short*) koopa_data, (koopa_width * koopa_height) / 2);
+}
+
+/* a struct for the koopa's logic and behavior */
+struct Koopa {
+    /* the actual sprite attribute info */
+    struct Sprite* sprite;
+
+    /* the x and y postion */
+    int x, y;
+
+    /* which frame of the animation he is on */
+    int frame;
+
+    /* the number of frames to wait before flipping */
+    int animation_delay;
+
+    /* the animation counter counts how many frames until we flip */
+    int counter;
+
+    /* whether the koopa is moving right now or not */
+    int move;
+
+    /* the number of pixels away from the edge of the screen the koopa stays */
+    int border;
+};
+
+/* initialize the koopa */
+void koopa_init(struct Koopa* koopa) {
+    koopa->x = 100;
+    koopa->y = 113;
+    koopa->border = 40;
+    koopa->frame = 0;
+    koopa->move = 0;
+    koopa->counter = 0;
+    koopa->animation_delay = 8;
+    koopa->sprite = sprite_init(koopa->x, koopa->y, SIZE_16_32, 0, 0, koopa->frame, 0);
+}
+
+/* move the koopa left or right returns if it is at edge of the screen */
+int koopa_left(struct Koopa* koopa) {
+    /* face left */
+    sprite_set_horizontal_flip(koopa->sprite, 1);
+    koopa->move = 1;
+
+    /* if we are at the left end, just scroll the screen */
+    if (koopa->x < koopa->border) {
+        return 1;
+    } else {
+        /* else move left */
+        koopa->x--;
+        return 0;
+    }
+}
+int koopa_right(struct Koopa* koopa) {
+    /* face right */
+    sprite_set_horizontal_flip(koopa->sprite, 0);
+    koopa->move = 1;
+
+    /* if we are at the right end, just scroll the screen */
+    if (koopa->x > (WIDTH - 16 - koopa->border)) {
+        return 1;
+    } else {
+        /* else move right */
+        koopa->x++;
+        return 0;
+    }
+}
+
+void koopa_stop(struct Koopa* koopa) {
+    koopa->move = 0;
+    koopa->frame = 0;
+    koopa->counter = 7;
+    sprite_set_offset(koopa->sprite, koopa->frame);
+}
+
+/* update the koopa */
+void koopa_update(struct Koopa* koopa) {
+    if (koopa->move) {
+        koopa->counter++;
+        if (koopa->counter >= koopa->animation_delay) {
+            koopa->frame = koopa->frame + 16;
+            if (koopa->frame > 16) {
+                koopa->frame = 0;
+            }
+            sprite_set_offset(koopa->sprite, koopa->frame);
+            koopa->counter = 0;
+        }
+    }
+
+    sprite_position(koopa->sprite, koopa->x, koopa->y);
+}
+
 /* the main function */
 int main() {
     /* we set the mode to mode 0 with bg0 and bg1 on */
-    *display_control = MODE0 | BG0_ENABLE | BG1_ENABLE;
+    *display_control = MODE0 | BG0_ENABLE | BG1_ENABLE | SPRITE_ENABLE | SPRITE_MAP_1D;
 
     /* setup the background 0 */
     setup_background();
 
+    /* setup the sprite image data */
+    setup_sprite_image();
+
+    
+
+    struct Koopa koopa;
+    koopa_init(&koopa);
+
     /* set initial scroll to 0 */
     int xscroll = 0;
-    int yscroll = 0;
 
     /* loop forever */
     while (1) {
-        /* scroll with the arrow keys */
-        if (button_pressed(BUTTON_DOWN)) {
-            yscroll++;
-        }
-        if (button_pressed(BUTTON_UP)) {
-            yscroll--;
+        /* update the koopa */
+        koopa_update(&koopa);
 
-        }
+        /* scroll with the arrow keys */
         if (button_pressed(BUTTON_RIGHT)) {
-            xscroll++;
+            if(koopa_right(&koopa)){
+                xscroll++;
+            }
         }
         if (button_pressed(BUTTON_LEFT)) {
-            xscroll--;
+            if(koopa_left(&koopa)){
+                xscroll--;
+            }
+        } else {
+            koopa_stop(&koopa);
         }
 
         /* wait for vblank before scrolling */
         wait_vblank();
         *bg0_x_scroll = xscroll;
-        *bg0_y_scroll = yscroll;
 
-        *bg1_x_scroll = xscroll * 2;
-        *bg1_y_scroll = yscroll *2;
+        *bg1_x_scroll = xscroll / 4;
+
+        sprite_update_all();
 
         /* delay some */
         delay(50);
